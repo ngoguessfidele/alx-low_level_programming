182b835a1a4f3dfd456eaf57456c582c705dce4f
Ngoga <ngoguessfidele@gmail.com>
2023-03-17 06:09:32 -0700

    puts fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* 
+ * This will print string followed by a new line
+ */
+
+int main(void)
+{
+	char str1[]="Programming is like building a multlingual puzzle\n";
+	puts(str1)
+
+	return 0;
+}
