e3ca66a79dede64e0dafe66281f22f005ef09287
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 19:16:06 +0200

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal- print diagonal line
+ * @n: integer
+ *
+ * Return: 0 success
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0, j;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
