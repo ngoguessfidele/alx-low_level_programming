7865538d5661a92063ab155b103ed6798cf11786
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 15:29:03 +0200

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers- print from 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	for (c = '0'; i <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
