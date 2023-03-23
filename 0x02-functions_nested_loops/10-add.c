0ae3b11a4fdd7fa12aaecded56852bbf7aee5989
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 09:48:24 +0200

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add- adds two integers
+ * @num1: first int
+ * @num2: second int
+ *
+ * Return: sum
+ */
+
+int add(int num1, int sum2)
+{
+	int num1;
+	int num2;
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
