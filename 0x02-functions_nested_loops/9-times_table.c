a97ba74b9799177af923ac0a065446a7d000ecf2
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 09:41:39 +0200

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *times_table- print the 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int r, c, prod, tens, ones;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for(c = 0; c <= 9; c++)
+		{
+			prod = r * c;
+			tens = prod / 10;
+			ones = prod % 10;
+
+			if (c == 0)
+			{
+				_putchar('0');
+			}
+			else if (prod < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
