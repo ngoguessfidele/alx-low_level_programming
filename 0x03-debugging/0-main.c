0103e613acecd6d538c7dfa569fe21c3a26191c5
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 20:35:21 +0200

    first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main-test fun that prints if integer ispositve of negative
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
