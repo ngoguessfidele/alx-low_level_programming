88ce5e7495146b5f2b549c7a8adec3183913388c
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 15:21:19 +0200

    product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul- multiple integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
