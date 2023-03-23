385b3d053b29e0af5715919f1e43a8d3ff5d5a2e
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 16:19:26 +0200

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line- print a lint 
+ * @n: integer
+ *
+ * Return: 0 always
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0);
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
