9a7fca892d5508ab386125f285f574a9aaea66e8
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 13:06:36 +0200

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main: this is the main
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	long int m, n, o, p;
+
+	n = 1;
+	o = 2;
+
+	for (m = 0; m <= 50; m++)
+	{
+		if (n != 20365011074)
+		{
+			printf("%ld, ", n);
+		}
+		else
+		{
+			printf("%ld, ", n);
+		}
+		p = n + o;
+		n = o;
+		o = p;
+	}
+	return (0);
+}
