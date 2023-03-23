9749abcd225c6ac97617ac91df1466d534109a39
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 15:53:33 +0200

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers- print numbers many times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{ 
+	int i;
+	int c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+				_putchar((c / 10) + 48);
+			_putchar((c % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
