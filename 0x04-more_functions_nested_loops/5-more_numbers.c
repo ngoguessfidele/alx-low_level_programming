b37c180e2c4ff742408a1535810a40f4553a969d
Ngoga <ngoguessfidele@gmail.com>
2023-03-23 16:01:50 +0200

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,10 @@ void more_numbers(void)
 		for (c = 0; c < 15; c++)
 		{
 			if (c >= 10)
+			{
 				_putchar((c / 10) + 48);
-			_putchar((c % 10) + 48);
+				_putchar((c % 10) + 48);
+			}
 		}
 		_putchar('\n');
 	}
