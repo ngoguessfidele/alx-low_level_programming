b248ad7d49e86632713f2cf783f8768b41d024ff
Ngoga <ngoguessfidele@gmail.com>
2023-03-27 12:16:07 +0200

    new

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts- prints string
+ * @str: str
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
