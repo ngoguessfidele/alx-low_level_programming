898d0b9818c97b7a1985cce4010e13319f812334
Ngoga <ngoguessfidele@gmail.com>
2023-03-27 11:38:50 +0200

    swaps numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int- swaps values of integers
+ * @a: pointer
+ * @b: another pointer
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n = *a;
+	*a = *b;
+	*b = x;
+}
