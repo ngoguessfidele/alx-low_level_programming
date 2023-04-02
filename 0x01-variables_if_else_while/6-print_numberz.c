c319465605f04d2d69b9417540d63591be3f030b
Ngoga <ngoguessfidele@gmail.com>
2023-04-02 17:32:48 +0200

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- print numbers
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
