8513e18aa5b4f396c62a8fa67af31216601a9072
Ngoga <ngoguessfidele@gmail.com>
2023-04-02 17:16:07 +0200

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- prints alphabets except q and e
+ *
+ * Return: 0 on success
+ */ 
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if ((c != 'q') && (c != 'e'))
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
