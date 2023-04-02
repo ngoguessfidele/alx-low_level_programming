b35c32820dc996c1dc0a79d57306c80c32b32e88
Ngoga <ngoguessfidele@gmail.com>
2023-04-02 16:56:28 +0200

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- prints in lowercase
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
