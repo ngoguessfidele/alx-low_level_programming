7e33e9e407102bb5afbc1de505bca6b29841005b
Ngoga <ngoguessfidele@gmail.com>
2023-04-02 15:16:05 +0200

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 		last_digit = n;
 	}
-	else if ((n >=10) || (n <= -10))
+	else if ((n >= 10) || (n <= -10))
 	{
 		last_digit = n % 10;
 	}
@@ -33,7 +33,8 @@ int main(void)
 	}
 	else if ((last_digit < 6) && (last_digit != 0))
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d", n, last_digit);
+		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }
