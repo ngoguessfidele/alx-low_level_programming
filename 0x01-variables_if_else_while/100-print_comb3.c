05c43c5388c01da054dba883b7f0191fe03490d8
Ngoga <ngoguessfidele@gmail.com>
2023-04-02 18:24:49 +0200

    new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main- comb of two digits
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int d1;
+	int d2;
+
+	for (d1 = 48; d1 < 58; d1++)
+	{
+		for(d2 = 49; d2 < 58; d2++)
+		{
+			if (d2 > d1)
+			{
+				putchar(d1);
+				putchar(d2);
+				if ((d1 != 56) || (d2 != 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
