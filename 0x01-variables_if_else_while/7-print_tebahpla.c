6fa2a7044363540b3746a7c0faa9c36c6cf30b02
Ngoga <ngoguessfidele@gmail.com>
2023-04-02 17:38:40 +0200

    new

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- print in reverse
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int c;
+	
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
