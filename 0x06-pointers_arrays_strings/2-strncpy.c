46261c2be32d1d77177d65ab5c6b0cbe2863e9e4
Ngoga <ngoguessfidele@gmail.com>
2023-04-04 09:34:47 -0700

    new

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy- copies a string
+ * @dest: destinatin
+ * @src: source
+ * @n: bytes
+ *
+ * Return: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\n'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
