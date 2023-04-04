e1e2da11bef1a838c4768383e3aa662fb7a2f29f
Ngoga <ngoguessfidele@gmail.com>
2023-04-04 10:03:44 -0700

    new

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp- compares the strings
+ * @s1: first string
+ * @s2: seconde string
+ * Return: 0 if equal
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, op = 0;
+
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 +i);
+		i**;
+	}
+	return (op);
+}
