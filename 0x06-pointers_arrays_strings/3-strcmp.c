a0c8f126960afe072826bdcda16199abaca57133
Ngoga <ngoguessfidele@gmail.com>
2023-04-04 10:15:20 -0700

    new

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
+	int i;
 
-	while (s1[i] != '\0' && j == 0)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		b = s1[i] - s2[i];
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-	return (j);
+	return (0);
 }
