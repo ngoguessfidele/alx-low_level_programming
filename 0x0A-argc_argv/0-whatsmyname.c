bc40d886b7c6b2c597b1cf4e86d3d13ed6826f46
Ngoga <ngoguessfidele@gmail.com>
2023-05-03 21:58:16 +0200

    new

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
