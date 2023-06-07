1528580f5fb76a3e169716cf342adf8c0dbcc319
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-07 23:12:45 +0200

    Third question

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion- string length
+ * @s:
+ *
+ * Return: integer len
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+	{
+		return (len);
+	}
+
+	len = 1 + _strlen_recursion(s + 1);
+
+	return(len);
+}
