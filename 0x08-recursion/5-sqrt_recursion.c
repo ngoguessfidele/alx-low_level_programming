457529b7b32729dc95235480d8daec918f514bae
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-08 02:18:00 +0200

    Sixth Task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion- the recursionfu
+ * @n: integer
+ * Return: integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	_sqrt(n, 1);
+}
+
+/**
+ * _sqrt- square root
+ * @n: integer
+ * @r: integer
+ * Return: integer
+ */
+
+int _sqrt(int n, int r)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (r * r > n)
+	{
+		return (-1);
+	}
+	if (r * r == n)
+	{
+		return (r);
+	}
+	_sqrt(n, r + 1);
+}
