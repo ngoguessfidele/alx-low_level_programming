9ce0ab98f7f9d0ab5eb27d3f07bc9c2936cc56c9
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-09 01:16:04 +0200

    Eight

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion- size of string
+ * @s: *string
+ * Return: recursion
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1- palindrome
+ * @s: *string
+ * @l: position
+ * Return: logic
+ */
+int _return(char *s, int len)
+{
+	if (len < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + l))
+	{
+		return (_return(s + 1, len - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome- palindrome
+ * @s: *string
+ * Return: recursion
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (_return(s, len - 1));
+}
