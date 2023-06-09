9c3ee2b8829f1f958ec2abcc4bfacc1ace297d04
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-10 00:11:10 +0200

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
