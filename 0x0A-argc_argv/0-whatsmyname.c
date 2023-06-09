58ab82e33285bda3227c5b5a86bca6f0c4681364
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-09 23:51:59 +0200

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", arg[0]);
+
+	return (0);
+}
