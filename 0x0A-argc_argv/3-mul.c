0132a592ef5a20b25e1d51725c2cadc07d5da1b8
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-10 00:35:24 +0200

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (0);
+	}
+	else
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+}
