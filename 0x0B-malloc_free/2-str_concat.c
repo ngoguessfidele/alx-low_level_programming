7b6d808813753fafd448926b50969410b3096088
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-14 01:36:26 +0200

    correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,28 +12,32 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	unsigned int i, j, len1, len2;
+	unsigned int i, j;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
 
-	while (*s1 != '\0')
+	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-	while (*s2 != '\0')
+	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
 	str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		str[i] = s1[i];
 	}
 	for (j = 0; s2[j] != '\0'; j++)
 	{
-		str[i + j] = s2[j];
+		str[i] = s2[j];
+		i++;
 	}
-	if (str == NULL)
-	{
-		return (NULL);
-	}
+	str[i] = '\0';
 	return (str)
 }
