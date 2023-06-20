96bf579997a269efd08638b24a7a7f4fa0a28a9d
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-20 04:37:43 +0200

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,8 +19,8 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-typedef struct dog dog_t;
+//dog_t *new_dog(char *name, float age, char *owner);
+//id free_dog(dog_t *d);
+//pedef struct dog dog_t;
 
 #endif
