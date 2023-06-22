8907fd921dda796b62f9499edd4a1ca54d452d9a
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-22 03:42:50 +0200

    c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,14 +3,15 @@
 /**
  * array_iterator- array iterator
  * @array: the array
- * size: size of the array
- * action: pointer to function
+ * @size: size of the array
+ * @action: function pointer
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-	if (array && action)
+
+	if (array != NULL && action != NULL && size > 0)
 	{
 		for (i = 0; i <= size; i++)
 		{
