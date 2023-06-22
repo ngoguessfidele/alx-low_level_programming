d6bcdbdf77b025a0e99f1a01bc8a571f084aeb6d
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-22 03:36:43 +0200

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator- array iterator
+ * @array: the array
+ * size: size of the array
+ * action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	if (array && action)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
