12b3e7beb313ed34821ec9621d7ddbaad8887331
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-23 03:08:59 +0200

    c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,12 +16,11 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	int num;
 
-	va_start(args, n);
-
 	if (n == 0)
 	{
 		return (0);
 	}
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
