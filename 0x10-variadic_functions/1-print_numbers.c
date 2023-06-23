398df57e0eaf13994a28727d461212f2b4818971
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-23 04:48:17 +0200

    c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,14 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (separator)
+		if (separator && i < n - 1)
 		{
 			printf("%s", separator);
 		}
 	}
-	printf("%d\n", va_arg(args, int));
+	printf("\n");
 	va_end(args);
 }
