6faf9eda179810a788ee76f66b5a3cce28fba1b3
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-24 04:31:24 +0200

    Last Task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,4 +10,27 @@
 
 int main(int argc, char *argv[])
 {
+	char *opcodes = (char*) main;
+	int n;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", opcodes[i] & 0xFF);
+		if (i < n - 1);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
