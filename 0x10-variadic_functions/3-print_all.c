fbadbc6feb791b0a3bda1d3cb94938cf64509d67
ngoguessfidele <ngoguessfidele@gmail.com>
2023-06-24 05:11:47 +0200

    correction

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,7 +8,7 @@
 void print_char(va_list args)
 {
 	char c = va_arg(args, char);
-	
+
 	printf("%c", c);
 }
 
@@ -63,4 +63,29 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int i = 0;
 	int j = 0;
-	char *separator = ", ";
+	char *separator = "";
+	options fun[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_s}
+	};
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4 && (format[i] != *(fun[j].specifier)))
+			j++;
+		if (j < 4)
+		{
+			printf("%s", separator);
+			fun[j].print_fun(args);
+			separator = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct _print- struct defining
+ * @specifier: what to print
+ * @print_fun: function pointer
+ */
+
+typedef struct _print
+{
+	char *specifier;
+	void (*print_fun)(va_list args);
+} options;
+
 #endif
