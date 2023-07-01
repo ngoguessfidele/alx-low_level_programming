cb0e8058e26752aab8584ac25bd4e76f3e1729ab
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-01 02:16:44 +0200

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list- prints a list
+ * @h: pointer to head
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] \"(nil)\"\n", 0);
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count += 1;
+	}
+	printf("[%d] %s\n", h->len, h->str);
+
+	return (count);
+}
