8b7e26bca50bd6d94c35ea73a20c8da983ead1fc
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-01 04:15:59 +0200

    correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@
 void __attribute__((constructor)) printMessage()
 {
 	printf("You're a beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n);
+	printf("I bore my house upon my back!\n");
 }
