9eb3ee670a903980dc9905fee77c6a96d05df323
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-01 04:19:34 +0200

    cor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,5 @@
 
 void __attribute__((constructor)) printMessage()
 {
-	printf("You're a beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}
