f0badf98fde6ac621c1ccd7e244eefb8f3992a29
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-06 07:15:18 -0700

    Task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint- add node the the begin..
+ * @head: head of list
+ * @n: data to add\
+ *
+ * Return: address of added node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *addNode;
+
+	addNode = malloc(sizeof(listint_t));
+	if (addNode == NULL)
+		return (NULL);
+	addNode->n = n;
+	addNode->next = *head;
+
+	return (addNode);
+}
