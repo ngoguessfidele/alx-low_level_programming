cfb73bf3f7866e746e55464bafd87b0f15c41486
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-06 10:30:28 -0700

    correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,11 +18,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
+	newnode->next = NULL;
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
 	current = *head;
-	while (count < idx && current != NULL)
+	while (count < idx - 1 && current != NULL)
 	{
 		count++;
 		current = current->next;
+	}
+	if (current == NULL)
+	{
+		free(newnode);
+		return (NULL);
 	}
 	newnode->next = current->next;
 	current->next = newnode;
