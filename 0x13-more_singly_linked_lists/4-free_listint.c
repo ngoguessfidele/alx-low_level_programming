8de46a1d463cc5f2535e4891f4fd0bee5fa4425d
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-06 08:04:05 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint- free list
+ * @head: head of list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *Next;
+	while (head != NULL)
+	{
+		Next = head->next;
+		free(head);
+		head = Next;
+	}
+}
