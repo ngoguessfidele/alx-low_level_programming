8039960ededec2bf573f5bdc33ee9167fb6f034c
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-06 06:47:10 -0700

    correnction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,12 @@
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
-	listint_t *current;
 
-	if (h == NULL)
-		return (0);
-
-	for (current = h; current != NULL; current = current->next)
+	while (h != NULL)
 	{
-		printf("%d\n", current->n);
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
-	return (count - 1);
+	return (count);
 }
