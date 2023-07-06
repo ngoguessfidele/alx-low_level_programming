c4b9a6c0410284351448d5e89c0e48f4a8d5065a
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-06 07:14:00 +0200

    Task 0 part 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_listint- print a list ofint
+ * @h: the list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count;
+	listint_t *next;
