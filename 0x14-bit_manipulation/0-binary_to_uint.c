58db9690d5aca15362fd1faa6d2d1734bcedafbf
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-10 23:05:50 +0200

    Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint- from binary to int
+ * @b: char of 1 and 0
+ *
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int uint = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i] != '0'; i++)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		uint = uint * 2 + (b[i] - '0');
+	}
+	return (uint);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
