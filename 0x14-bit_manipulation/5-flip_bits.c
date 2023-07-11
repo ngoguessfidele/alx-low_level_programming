b7dd5286b00c5eb3af2f511c4aa49cbf4608011b
ngoguessfidele <ngoguessfidele@gmail.com>
2023-07-11 04:57:57 +0200

    betty

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits: number of bits
+ * flip_bits- number of bits to change
  * @n: first int
  * @m: second int
  *
@@ -11,6 +11,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int count = 0;
+
 	while (n || m)
 	{
 		if ((n & 1) != (m & 1))
