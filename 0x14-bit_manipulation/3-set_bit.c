1768ca014c9ce0b16cb97fbca5f7aec047887ae8
ngoguessfidele <ngoguessfidele@gmail.com>
2023-08-03 16:16:08 +0200

    Taask 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit- set bit at index
+ * @n: number
+ * @index: index
+ * Return: 1 on success, or -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bits = sizeof(unsigned long int) * 8;
+	unsigned long int mask = 1UL << index;
+
+	if (index >= bits)
+		return (-1);
+	*n |= mask;
+	return (-1);
+}
