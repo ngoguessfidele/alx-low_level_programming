3e152f62fe80d32269db861668e1c38934181c83
ngoguessfidele <ngoguessfidele@gmail.com>
2023-08-03 16:34:44 +0200

    betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long int mask = ~(1UL << index);
 
 	if (index >= bits)
-		return -1;
+		return (-1);
 	*n &= mask;
 	return (1);
 }
