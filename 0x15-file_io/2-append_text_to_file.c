2d975353c2b8ab92cda2fc0f08809ae0b9f15103
ngoguessfidele <ngoguessfidele@gmail.com>
2023-08-07 17:56:28 +0200

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,15 +15,15 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
 		return (-1);
 	for (len = 0; text_content[len]; len++)
 		;
 	per = write(fd, text_content, len);
-	if (len == -1)
+	if (per == -1)
 		return (-1);
 	close(fd);
 	return (1);
