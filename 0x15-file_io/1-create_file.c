1f6c4c975ec4faae691b569d2e7664e0e1057bfd
ngoguessfidele <ngoguessfidele@gmail.com>
2023-08-07 09:15:55 +0200

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_file- creates a file
+ * @filename: file name
+ * @text_content: content to write
+ * Return: 1 on success, or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		write(fd, text_content, strLen(text_content));
+	close(fd);
+	return (1);
+}
