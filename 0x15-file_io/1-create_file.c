9091a75d2009887e1c37a210f4fe30477ca37a34
ngoguessfidele <ngoguessfidele@gmail.com>
2023-08-07 17:32:12 +0200

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,6 +10,8 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	int len;
+	int per;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,7 +20,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	write(fd, text_content, strLen(text_content));
+	for (len = 0; text_content[len]; len++)
+		;
+	per =  write(fd, text_content, len);
+	if (per == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
