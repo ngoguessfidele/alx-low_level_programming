38ededa040f671bd85dbabc46be94a3971d1749f
ngoguessfidele <ngoguessfidele@gmail.com>
2023-08-08 07:35:09 +0200

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,12 +19,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
-	for (len = 0; text_content[len]; len++)
-		;
-	per = write(fd, text_content, len);
-	if (per == -1)
-		return (-1);
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+		per = write(fd, text_content, len);
+		if (per == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
