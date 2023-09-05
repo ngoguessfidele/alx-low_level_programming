261dfbafe88ecfb1580ec7bbd46d81de81e3a795
ngoguessfidele <ngoguessfidele@gmail.com>
2023-09-05 05:08:03 +0200

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,26 +20,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters + 1));
+	buffer = malloc(sizeof(char) * (letters));
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
 
 	red = read(fd, buffer, letters);
-	if (read < 0)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-	buffer[red] = '\0';
 	wrote = write(STDOUT_FILENO, buffer, red);
 	free(buffer);
 	close(fd);
-	if (wrote < 0 || wrote != red)
-		return (0);
 
 	return (red);
 }
