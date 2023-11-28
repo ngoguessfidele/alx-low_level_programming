6a4609b432a5381b02314b606c08b1a8e32d3f19
ngoguessfidele <ngoguessfidele@gmail.com>
2023-11-28 03:23:57 +0200

    hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: size
+ * Return: the hash table or NULL 
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *tab;
+	hash_node_t **arr;
+	unsigned long int i;
+
+	tab = malloc(sizeof(hash_table_t));
+	if (tab == NULL)
+		return (NULL);
+
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = NULL;
+
+	tab->array = arr;
+	tab->size = size;
+
+	return (tab);
+}
